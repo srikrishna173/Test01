ac6888dd49be3d7e971da540738dcd1f8a7848f5
srikrishna.jena <srikrishna.jena@BDC6-D-3WDMX52.dir.svc.accenture.com>
2017-05-30 18:25:36 +0530

    Commit

---
+++ GitConfig/src/test/java/GitConfiguration/GitTest.java
@@ -0,0 +1,30 @@
+package GitConfiguration;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+public class GitTest {
+	
+	WebDriver driver;
+	
+	@BeforeMethod
+	public void launchURL() {
+		String Chromepath = ClassLoader.getSystemResource("chromedriver.exe").getFile();
+		System.setProperty("webdriver.chrome.driver", Chromepath);
+		driver = new ChromeDriver();
+		driver.get("http://localhost:8080/");
+	}
+
+	@Test
+	public void gitTest() {
+		System.out.println("Test Git");
+	}
+	
+	@AfterMethod
+	public void closeBrowser() {
+		driver.close();
+	}
+}
