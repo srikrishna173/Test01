8415d0e4ff55d5eb02024bd9bf20eb5c13c321cb
srikrishna.jena <srikrishna.jena@BDC6-D-3WDMX52.dir.svc.accenture.com>
2017-05-31 18:37:44 +0530

    dasdf

--- GitConfig/src/test/java/GitConfiguration/GitTest.java
+++ GitConfig/src/test/java/GitConfiguration/GitTest.java
@@ -1,14 +1,14 @@
 package GitConfiguration;
 
-import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.remote.RemoteWebDriver;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
 public class GitTest {
 	
-	WebDriver driver;
+	RemoteWebDriver driver;
 	
 	@BeforeMethod
 	public void launchURL() {
