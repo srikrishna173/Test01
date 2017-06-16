1056a1d62b62f0f978f6bf1816dd58cdf9fdfcf5
srikrishna.jena <srikrishna.jena@BDC6-D-3WDMX52.dir.svc.accenture.com>
2017-06-16 19:18:05 +0530

    updated code

--- GitConfig/src/test/java/GitConfiguration/GitTest.java
+++ GitConfig/src/test/java/GitConfiguration/GitTest.java
@@ -1,32 +1,65 @@
 package GitConfiguration;
 
+import java.io.File;
+
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.remote.RemoteWebDriver;
+import org.openqa.selenium.firefox.FirefoxBinary;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.firefox.FirefoxProfile;
+import org.openqa.selenium.interactions.Actions;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
 public class GitTest {
 	
-	RemoteWebDriver driver;
+	WebDriver driver;
 	
 	@BeforeMethod
 	public void launchURL() {
 		
 		//String Chromepath = Thread.currentThread().getContextClassLoader().getResource("chromedriver.exe").getPath();
 		//System.setProperty("webdriver.chrome.driver", "/GitRepo/GitConfig/src/test/resources/chromedriver.exe");
-		System.setProperty("webdriver.chrome.driver",ClassLoader.getSystemResource("chromedriver.exe").getFile().replaceAll("%20", " "));
+		System.setProperty("webdriver.chrome.driver", classloader("chromedriver.exe"));
+		
+		//‪C:\Users\srikrishna.jena\Downloads\Firefox Setup Stub 53.0.3.exe
 		driver = new ChromeDriver();
-		driver.get("http://localhost:8080/");
+		
+//		File pathBinary = new File("C:/Users/srikrishna.jena/Downloads/Firefox Setup Stub 53.0.3.exe");
+//		FirefoxBinary firefoxBinary = new FirefoxBinary(pathBinary);
+//		FirefoxProfile firefoxProfile = new FirefoxProfile();       
+//		WebDriver driver = new FirefoxDriver(firefoxBinary, firefoxProfile);
+		
+//		
+//		File pathToBinary = new File("C:/Users/srikrishna.jena/Downloads/Firefox Setup Stub 53.0.3.exe");
+//		FirefoxBinary ffBinary = new FirefoxBinary(pathToBinary);
+//		FirefoxProfile firefoxProfile = new FirefoxProfile();
+//		System.setProperty("webdriver.gecko.driver","C:/Users/srikrishna.jena/Downloads/geckodriver.exe");       
+//		WebDriver driver = new FirefoxDriver(ffBinary,firefoxProfile);
+		
+		driver.get("http://www.guru99.com/");
+		driver.manage().window().maximize();
 	}
+	
+	public String classloader(String element) {
+		String path = ClassLoader.getSystemResource(element).getFile().replaceAll("%20", " ");
+		return path;
+		
+	}
+		
 
 	@Test
 	public void gitTest() {
 		System.out.println("Test Git");
+		Actions action = new Actions(driver);
+		action.sendKeys(Keys.ALT, "f").sendKeys(Keys.ARROW_UP).sendKeys(Keys.ENTER).build().perform();
+		System.out.println("Done");
 	}
 	
 	@AfterMethod
 	public void closeBrowser() {
-		driver.close();
+
 	}
 }
